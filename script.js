3c60c490ba8a298d69d00a3bc9c9f038c74e68eb
ninokhutsi <nino.khutsiberidze16b@geolab.edu.ge>
2021-07-02 16:03:45 +0400

    add burtuliko eye animation

---
+++ script.js
@@ -0,0 +1,11 @@
+let balls = document.getElementsByClassName("eyeball");
+document.onmousemove = function(event){
+    let x = event.clientX * 100 / window.innerWidth + "%";
+    let y = event.clientY * 100 / window.innerHeight + "%";
+
+    for(let i = 0; i < 2; i++){
+        balls[i].style.left = x;
+        balls[i].style.top = y;
+        balls[i].style.transform = "translate(-"+x+", -"+y+")";
+    }
+}
